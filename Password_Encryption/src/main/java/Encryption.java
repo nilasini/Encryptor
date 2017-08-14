7acaa361f831658ceb318570134ea669f125db1d
nilasini <nilathiru93@gmail.com>
2017-08-14 14:42:44 +0530

    improve the implementation

--- Password_Encryption/src/main/java/Encryption.java
+++ Password_Encryption/src/main/java/Encryption.java
@@ -2,43 +2,57 @@ import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.Console;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.spec.KeySpec;
 import java.util.Arrays;
-import java.util.List;
 
 public class Encryption {
 
     private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String SALT = "84B03D034B409D4E";
 
-    public static String encode(String secret,
-                                String cipherKey)
-            throws Exception {
+    public static String encode(String secret, char[] cipherKey) throws Exception {
 
-        SecretKey key = new SecretKeySpec(cipherKey.getBytes(), "AES");
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(cipherKey, SALT.getBytes(StandardCharsets.UTF_8), 65536, 128);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKey secretKeySpec = new SecretKeySpec(tmp.getEncoded(), "AES");
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
 
         byte[] iv = new byte[cipher.getBlockSize()];
 
         IvParameterSpec ivParams = new IvParameterSpec(iv);
 
-        cipher.init(Cipher.ENCRYPT_MODE, key, ivParams);
-        byte[] encryptedVal = cipher.doFinal(secret.getBytes("UTF-8"));
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParams);
+        byte[] encryptedVal = cipher.doFinal(secret.getBytes(StandardCharsets.UTF_8));
         byte[] encodedVal = new Base64().encode(encryptedVal);
 
-        return new String(encodedVal, "UTF-8");
+        return new String(encodedVal, StandardCharsets.UTF_8);
     }
 
-    public static void main (String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+
         String encryptedVal;
-        encryptedVal = encode(args[0], args[1]);
-        Files.write(Paths.get("../secret_key.txt"), encryptedVal.getBytes());
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("Couldn't get Console instance");
+            System.exit(0);
+        }
+        char passwordArray[] = console.readPassword("Please Enter a password you want to use for the encryption: ");
+        encryptedVal = encode(args[0], passwordArray);
+        Files.write(Paths.get("./encrypted_password.txt"), encryptedVal.getBytes());
+        Arrays.fill(passwordArray, (char)0);
     }
 }
